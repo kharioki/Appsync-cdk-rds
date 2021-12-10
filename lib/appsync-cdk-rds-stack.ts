df68d3e5a0962e4492d4938de9bc332f6cc7db01
tony <antolisah@gmail.com>
2021-12-10 18:26:47 +0300

    create lambda functions and resolvers

--- lib/appsync-cdk-rds-stack.ts
+++ lib/appsync-cdk-rds-stack.ts
@@ -21,5 +21,57 @@ export class AppsyncCdkRdsStack extends cdk.Stack {
         },
       },
     });
+
+    // Create the VPC needed for the Aurora Serverless DB cluster
+    const vpc = new ec2.Vpc(this, 'BlogAppVpc');
+    // Create the Serverless Aurora DB cluster; set the engine to Postgres
+    const cluster = new rds.ServerlessCluster(this, 'AuroraBlogCluster', {
+      engine: rds.DatabaseClusterEngine.AURORA_POSTGRESQL,
+      parameterGroup: rds.ParameterGroup.fromParameterGroupName(this, 'ParameterGroup', 'default.aurora-postgresql10'),
+      defaultDatabaseName: 'BlogDB',
+      vpc,
+      scaling: { autoPause: cdk.Duration.seconds(0) }, // Optional. If not set, then instance will pause after 5 minutes
+    });
+
+    // Create the Lambda function that will map GraphQL operation into Postgres
+    const postFn = new lambda.Function(this, 'MyFunction', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'index.handler',
+      code: new lambda.AssetCode('lambda-fns'),
+      memorySize: 1024,
+      environment: {
+        CLUSTER_ARN: cluster.clusterArn,
+        SECRET_ARN: cluster.secret?.secretArn || '',
+        DB_NAME: 'BlogDB',
+        AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      },
+    });
+
+    // Grant access to the cluster from the Lambda function
+    cluster.grantDataApiAccess(postFn);
+    // Set the new Lambda function as a data source for the AppSync API
+    const lambdaDs = api.addLambdaDataSource('LambdaDataSource', postFn);
+
+    // Define resolvers to map GraphQL operations to the Lambda function
+    lambdaDs.createResolver({
+      typeName: 'Query',
+      fieldName: 'listPosts',
+    });
+    lambdaDs.createResolver({
+      typeName: 'Query',
+      fieldName: 'getPostById',
+    });
+    lambdaDs.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'createPost',
+    });
+    lambdaDs.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'updatePost',
+    });
+    lambdaDs.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'deletePost',
+    });
   }
 }
