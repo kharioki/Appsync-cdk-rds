9418c46018ea3ffc44e028b04b1d3b341a3dddf0
tony <antolisah@gmail.com>
2021-12-10 16:09:30 +0300

    Initial commit

---
+++ test/appsync-cdk-rds.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as AppsyncCdkRds from '../lib/appsync-cdk-rds-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/appsync-cdk-rds-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AppsyncCdkRds.AppsyncCdkRdsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
