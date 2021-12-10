726b0899e5fdc366c8962dcad46223b7a5eeced2
tony <antolisah@gmail.com>
2021-12-10 17:16:57 +0300

    define appsync api

--- lib/appsync-cdk-rds-stack.ts
+++ lib/appsync-cdk-rds-stack.ts
@@ -1,15 +1,25 @@
 import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as rds from '@aws-cdk/aws-rds';
+import * as appsync from '@aws-cdk/aws-appsync';
 
 export class AppsyncCdkRdsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AppsyncCdkRdsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create the AppSync API
+    const api = new appsync.GraphqlApi(this, 'Api', {
+      name: 'cdk-blog-appsync-api',
+      schema: appsync.Schema.fromAsset('graphql/schema.graphql'),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.API_KEY,
+          apiKeyConfig: {
+            expires: cdk.Expiration.after(cdk.Duration.days(365))
+          }
+        },
+      },
+    });
   }
 }
